9b67d48eb6d0ba12e7d401e61f7020c313e55e97
fpemba <fatuma.pemba@yahoo.com>
2020-03-09 20:30:16 -0400

    added conditional statements to change color of button

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
-
 let autoClick = 0;
 let collectiveCulminationCompounderClick = 0;
 let multiplier = 1.2;
-let power=0;
-let companionCost=100;
-let costOfCollectiveCulminationCompounder=10;
-let priceMultiplier=.10;
-let cookiecount =Math.pow(multiplier, power);
+let power = 0;
+let companionCost = 100;
+let costOfCollectiveCulminationCompounder = 10;
+let priceMultiplier = .10;
+let cookiecount = Math.pow(multiplier, power);
 let value = 0;
+
 function update() {
 
     document.querySelector('#cookie').value = Math.floor(cookiecount)
@@ -15,14 +15,18 @@ function update() {
 
     document.title = Math.floor(cookiecount) + "  Cookies";
 
-    document.getElementById('numberOfClickingCompanion').innerHTML = "You Have " + autoClick + " Clicking Companions.";
+    document.querySelector('#numberOfClickingCompanion').innerHTML = "You Have " + autoClick + " Clicking Companions.";
 
-    document.getElementById('costOfClickingCompanion').innerHTML = "Cost: " + companionCost + " Cookies";
+    document.querySelector('#costOfClickingCompanion').innerHTML = "Cost: " + companionCost + " Cookies";
 
-    document.getElementById('costOfCollectiveCulminationCompounder').innerHTML = "Cost: " + costOfCollectiveCulminationCompounder + " Cookies";
+    document.querySelector('#costOfCollectiveCulminationCompounder').innerHTML = "Cost: " + costOfCollectiveCulminationCompounder + " Cookies";
 
-    document.getElementById('numberOfCollectiveCulminationCompounders').innerHTML = "You Have " + collectiveCulminationCompounderClick + " Collective Culmination Compounders.";
+    document.querySelector('#numberOfCollectiveCulminationCompounders').innerHTML = "You Have " + collectiveCulminationCompounderClick + " Collective Culmination Compounders.";
     document.querySelector('#cookiesPerSecond').innerHTML = Math.round((autoClick * multiplier)) + " cookies per second";
+
+    document.querySelector("#purchaseClickingCompanion").style.backgroundColor = (cookiecount < 100) ? "gray" : "white";
+    document.querySelector("#purchaseCollectiveCulminationCompounder").style.backgroundColor = (cookiecount < 10) ? "gray" : "white";
+
 
 };
 
@@ -31,14 +35,14 @@ function timer() {
     cookiecount = cookiecount + autoClick;
     cookiecount = cookiecount + collectiveCulminationCompounderClick;
     update()
-   
+
 };
 setInterval(timer, 1000)
 
 
 function addCookie() {
 
-    cookiecount+=Math.pow(multiplier, power);
+    cookiecount += Math.pow(multiplier, power);
     update()
 };
 
@@ -55,7 +59,7 @@ function purchaseClickingCompanion() {
     if (cookiecount >= companionCost) {
         cookiecount = cookiecount - companionCost;
         autoClick = autoClick + 1;
-        companionCost+=Math.floor((companionCost*.10));
+        companionCost += Math.floor((companionCost * .10));
         update()
     }
 }
@@ -65,10 +69,10 @@ function purchaseCollectiveCulminationCompounder() {
     if (cookiecount >= costOfCollectiveCulminationCompounder) {
         cookiecount = cookiecount - costOfCollectiveCulminationCompounder;
         collectiveCulminationCompounderClick = collectiveCulminationCompounderClick + 1;
-        costOfCollectiveCulminationCompounder+=Math.round(costOfCollectiveCulminationCompounder*priceMultiplier);
+        costOfCollectiveCulminationCompounder += Math.round(costOfCollectiveCulminationCompounder * priceMultiplier);
         power++;
-        cookiecount+=value;
-        priceMultiplier+=.10;
+        cookiecount += value;
+        priceMultiplier += .10;
         update()
 
     };
